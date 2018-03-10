17a70ee1a055915e5f160fba490c98e83496d464
czy <752404737@qq.com>
2018-03-10 12:24:45 +0800

    czy1

---
+++ app/src/main/java/com/czy/newstronger/MainActivity.java
@@ -0,0 +1,14 @@
+package com.czy.newstronger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //czy1
+    }
+}
